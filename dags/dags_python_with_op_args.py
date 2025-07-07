f06453a7902538459cdfd9ca3e6583a22a55f1d5
song-757 <hyoarang72@daum.net>
2025-07-07 11:52:16 +0900

    파이썬 op args 테스트

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,21 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 6, 24, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+   
+) as dag:
+    task_get_sftp =  PythonOperator(
+        task_id = 'regist_t1',
+        python_callable=regist
+    )
+
+
+    regist_t1
