09c3a7a9f6e61a659c53f67b4746062a22937c03
song-757 <hyoarang72@daum.net>
2025-07-07 15:22:14 +0900

    catchup  true 변경 처리

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
-    catchup=False,
+    catchup=True,
     dagrun_timeout=datetime.timedelta(minutes=60),
    
 ) as dag:
