aab1e364726d37d4d967e7972adc172afa6cb6ef
song-757 <hyoarang72@daum.net>
2025-07-07 16:36:05 +0900

    macro 날짜 계산

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,20 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from 
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_task_1 = BashOperator(
+        task_id ='bash_task_1',
+        env = {
+            'START_DATE':'{{data_interval_start.in_timezone("Asia/Seoul") | ds}}',
+            'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul")-macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
+        },
+        bash_command = 'echo "START_DATE:"$START_DATE && echo "END_DATE:" $END_DATE"'
+    )
---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,20 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from common.common_func import regist2
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg2",
+    schedule="10 0 * * 6#2",
+    start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_task_2 = BashOperator(
+        task_id ='bash_task_2',
+        env = {
+            'START_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
+            'END_DATE':'{{(data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        },
+        bash_command = 'echo "START_DATE:"$START_DATE && echo "END_DATE:" $END_DATE"'
+    )
