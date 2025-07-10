fe3ea3d4e03882d439215acab7e4d45df177034e
song-757 <hyoarang72@daum.net>
2025-07-10 16:37:54 +0900

    Python Bash Operator간 xcom 전송

---
+++ dags/dag_bash_python_xcom.py
@@ -0,0 +1,34 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.decorators import task
+with DAG(
+    dag_id="dag_bash_python_xcom",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 6, 24, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+   
+) as dag:
+
+    bash_push = BashOperator(
+        task_id = 'bash_push',
+        bash_command="echo PUSH_START"
+                        '{{ti.xcom_push(key="bash_pushed",value="200")}} && '
+                        'echo PUSH_COMPLETED'
+    )
+
+
+    @task
+    def python_pull_xcom(**kwargs):
+        ti = kwargs['ti']
+        status_value = ti.xcom_pull(key="bash_pushed")
+        return_value = ti.xcom_pull(task_ids='bash_push')
+        result_dict = {'status':'good','data':[1,2,3],'options_cnt':100}
+        print('status_value:'+str(status_value))
+        print('return_value:'+str(return_value))
+    
+    bash_push >> python_pull_xcom
--- dags/dag_python_bash_xcom.py
+++ dags/dag_python_bash_xcom.py
@@ -1,10 +1,10 @@
 import datetime
 import pendulum
 from airflow.sdk import DAG
-from airflow.decorators import task
+
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.python import PythonOperator
-
+from airflow.decorators import task
 with DAG(
     dag_id="dag_python_bash_xcom",
     schedule="30 6 * * *",
@@ -13,20 +13,20 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=60),
    
 ) as dag:
-    python_push =  PythonOperator(
-        task_id = 'python_push',
-        result_dict = {'status':'good','data':'[1,2,3]','options_cnt':'10'}
+    @task
+    def python_push_xcom():
+        result_dict = {'status':'good','data':[1,2,3],'options_cnt':100}
         return result_dict
-    )
+    
 
     bash_pull = BashOperator(
         task_id = 'bash_pull',
         env ={
-            'STATUS':'{{ti.xcom_pull(task_ids="python_push")["status"]}}',
-            'DATA':'{{ti.xcom_pull(task_ids="python_push")["data"]}}',
-            'OPTIONS':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}'
+            'STATUS':'{{ti.xcom_pull(task_ids="python_push_xcom")["status"]}}',
+            'DATA':'{{ti.xcom_pull(task_ids="python_push_xcom")["data"]}}',
+            'OPTIONS':'{{ti.xcom_pull(task_ids="python_push_xcom")["options_cnt"]}}'
         },
         bash_command="echo $STATUS && echo $DATA && echo $OPTIONS "
     )
 
-    python_push >> bash_pull
+    python_push_xcom >> bash_pull
