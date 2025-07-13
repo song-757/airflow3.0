607b08fa2c7855c60289add7b21ae308787ac7cb
song-757 <hyoarang72@daum.net>
2025-07-13 23:14:54 +0900

    Variable 테스트

---
+++ dags/dags_bash_variable.py
@@ -0,0 +1,27 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id="dags_bash_variable",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 6, 24, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    var_value = Variable.get("sameple_key")
+
+    bash_var_1 = BashOperator(
+        task_id ="bash_var_1"
+        bash_command =f"echo variable:{var_value}"
+    )
+
+    bash_var_2 = BashOperator(
+        task_id ="bash_var_2"
+        bash_command =f"echo variable:{{var.value.sample_key}}"
+    )
+
+    bash_var_1 >> bash_var_2
