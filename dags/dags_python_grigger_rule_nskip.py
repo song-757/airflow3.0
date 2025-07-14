cf49c7125b4c1e663a2a82c13f9af6cdcbf5710f
song-757 <hyoarang72@daum.net>
2025-07-14 22:01:23 +0900

    오타수정

--- dags/dags_python_grigger_rule_nskip.py
+++ dags/dags_python_grigger_rule_nskip.py
@@ -34,7 +34,7 @@ with DAG(
 
     @task(task_id='task_b')
     def task_b():
-        raise AirflowException('python_upstream_1 exception')
+        print('정상처리')
 
     @task(task_id='task_c')
     def task_c():
