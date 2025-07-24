489cf34bd0dd6b8cf59ca5fd1850c0f271b813f3
song-757 <hyoarang72@daum.net>
2025-07-24 13:16:09 +0900

    ai소스

--- dags/dags_test_hello.py
+++ dags/dags_test_hello.py
@@ -2,13 +2,15 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+# DAG 정의
 dag = DAG(
     dag_id="dag_test_hello",
     start_date=datetime(2024, 1, 1),
-    schedule_interval="@once",
+    schedule="@once",        # ✅ schedule_interval → schedule 로 수정
     catchup=False
 )
 
+# DAG에 태스크 추가
 with dag:
     t1 = BashOperator(
         task_id="print_hello",
