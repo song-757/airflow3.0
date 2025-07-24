a11cbc44c2291648c08fb00e6d373b3f1fb389bd
song-757 <hyoarang72@daum.net>
2025-07-24 21:49:39 +0900

    다시원점으로

--- dags/dag_python_postgres_insert.py
+++ dags/dag_python_postgres_insert.py
@@ -1,25 +1,36 @@
 import datetime
 import pendulum
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 
-def insert_postgress(ip, port, dbname, user, pwd, **kwargs):
-    print('실행되나')
+from airflow.providers.standard.operators.python import PythonOperator
 
-# ✅ 전역에 선언!
-dag = DAG(
+with DAG(
     dag_id="dag_python_postgres_insert",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2025, 6, 24, tz="Asia/Seoul"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-)
+) as dag:
+    def insert_postgress(ip,port,dbname,user,pwd,**kwargs):
+        import psycopg2
+        from contextlib import closing
+        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user,password=pwd,port=int(port))) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg='insert 수행'
+                sql='insert into py_opr_drct_insrt values(%s,%s,%s,%s);'
+                cursor.execute(sql,(dag_id,task_id,run_id,msg))
+                conn.commit()
+        print('실행되나');
 
-with dag:
+
     insert_postgress_task = PythonOperator(
-        task_id='insert_postgress_task',
-        python_callable=insert_postgress,
-        op_args=['172.28.0.3', '5432', 'song', 'song', 'song']
+        task_id ='insert_postgress_task',
+        python_callable= insert_postgress,
+        op_args=['172.28.0.3','5432','song','song','song'] 
     )
+
 
     insert_postgress_task
