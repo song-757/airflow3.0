e939e8d656b5b8f13e0536db213807019c350cb7
song-757 <hyoarang72@daum.net>
2025-07-24 13:55:10 +0900

    ai5

--- dags/dags_test_hello.py
+++ dags/dags_test_hello.py
@@ -5,7 +5,7 @@ from datetime import datetime
 dag = DAG(
     dag_id="dag_test_hello",
     start_date=datetime(2024, 1, 1),
-    schedule_interval="@once",
+    schedule ="@once",
     catchup=False
 )
 
