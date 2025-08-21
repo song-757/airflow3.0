d6eae5c87b0f5babdd220ab9220b43ca096ce22d
song-757 <hyoarang72@daum.net>
2025-08-21 14:36:43 +0900

    default_args 테스트

---
+++ dags/dags_email_on_failure.py
@@ -0,0 +1,40 @@
+import datetime
+import pendulum
+from airflow.sdk import DAG
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.decorators import task
+from airflow.exceptions import AirflowException
+import pendulum
+from datetime import timedelta
+from airflow.models import Variable
+
+email_str= Variable.get('email_target')
+email_lst = [email.strip() fro email in email_str.split(',')]
+
+
+with DAG(
+    dag_id="dags_email_on_failure",
+    start_date=pendulum.datetime(2025, 6, 24, tz="Asia/Seoul"),
+    catchup=False,
+    schedule="30 6 * * *",
+    dagrun_timeout=timedelta(minute=2)
+    default_args={
+        'email_on_failure': True,
+        'email' : email_list
+    }
+) as dag:
+    @task(task_id='python_fail')
+    def python_task_func():
+        raise AirflowException('error occurred')
+    python_task_func()
+
+    bash_fail = BashOperator(
+        task_id = 'bash_fail',
+        bash_command = 'exit 1'
+    )    
+
+    bash_success = BashOperator(
+        task_id = 'bash_fail',
+        bash_command = 'exit 0'
+    )
