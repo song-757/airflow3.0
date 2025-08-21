212ef6877ff67cb20715d91dcfbe73a8e0d948e4
song-757 <hyoarang72@daum.net>
2025-08-22 07:36:41 +0900

    오류수정

--- dags/dags_time_sensor.py
+++ dags/dags_time_sensor.py
@@ -1,16 +1,16 @@
 import pendulum
-from airflow.sdk import DAG
+from airflow import DAG
 
 from airflow.sensors.date_time import DateTimeSensorAsync
 
 with DAG(
     dag_id="dags_time_sensor",
-    schedule="30 6 * * *",
-    start_date=pendulum.datetime(2025, 8, 22, 0,0,0),
-    end_date=pendulum.datetime(2025, 8, 22, 1,0,0),
+    schedule="*/10 * * * *",
+    start_date=pendulum.datetime(2025, 8, 22, 0,0,0, tz= pendulum.timezone("Asia/Seoul")),
+    end_date=pendulum.datetime(2025, 8, 22, 1,0,0, tz= pendulum.timezone("Asia/Seoul")),
     catchup=True,
 ) as dag:
     sync_sensor = DateTimeSensorAsync(
         task_id = "sync_sensor",
-        target_time="""{{ macros.datetime.utcnow() +macros.timedelta(minute=5)}}"""
+        target_time="""{{ macros.datetime.utcnow() +macros.timedelta(minutes=5)}}"""
     )
